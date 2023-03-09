f82fbdf382510ef25178c1a612192185391e7821
Szymon Tymcio <s.tymcio98@gmail.com>
2023-03-09 12:55:44 +0100

    phonebook ex 2.8

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,30 +1,45 @@
 import { useState } from "react";
 import "./App.css";
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "242-543-643" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNewPerson = (event) => {
     event.preventDefault();
     const Obj = {
       name: newName,
+      number: newNumber,
     };
-    persons.filter((person) => person.name === newName).length === 0
-      ? setPersons(persons.concat(Obj))
-      : alert(`Name ${newName} already exists`);
+    const phoneNumberRegex = /^\d{3}-\d{3}-\d{3}$/;
+    persons.filter((person) => {
+      return person.name === newName || person.number === newNumber;
+    }).length === 0
+      ? phoneNumberRegex.test(newNumber)
+        ? setPersons(persons.concat(Obj))
+        : alert("Incorect phone number form, should look like XXX-XXX-XXX")
+      : alert(`Name or Number already exists`);
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
   };
-
+  const handleNewPhone = (event) => {
+    const tempNumber = event.target.value;
+    setNewNumber(tempNumber);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleNewPerson}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
+          <br />
+          number: <input value={newNumber} onChange={handleNewPhone} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -32,8 +47,10 @@ const App = () => {
       </form>
       <div>
         <h2>Numbers</h2>
-        {persons.map((person) => (
-          <p key={person.name}>{person.name}</p>
+        {persons.map((person, index) => (
+          <p key={person.name}>
+            {`${index + 1}.  ${person.name}  ${person.number}`}
+          </p>
         ))}
       </div>
     </div>
