84fc4f70194b958f7a2092989b96ebc6c7cd650e
Szymon Tymcio <s.tymcio98@gmail.com>
2023-03-09 11:00:25 +0100

    phonebook ex 2.6

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNewPerson = (event) => {
+    event.preventDefault();
+    const Obj = {
+      name: newName,
+    };
+    setPersons(persons.concat(Obj));
+    setNewName("");
+  };
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <div>
+        <h2>Numbers</h2>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
